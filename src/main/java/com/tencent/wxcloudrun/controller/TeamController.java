7f248bce18554f1b124106d1267edef451b90fc9
keisean <244838053@qq.com>
2025-11-22 23:14:58 +0800

    feat: Add TeamController with APIs for team creation, joining, and management.

--- src/main/java/com/tencent/wxcloudrun/controller/TeamController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/TeamController.java
@@ -11,9 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.validation.Valid;
 
